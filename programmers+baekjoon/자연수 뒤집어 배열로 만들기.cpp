145b656426f7fd4a0ae20d919839f87451aabcd2
junhyunpark <wordjun@naver.com>
2020-09-30 23:49:55 +0900

    Sept 30th

---
+++ programmers+baekjoon/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    string str = to_string(n);
+    for (int i = str.length() - 1; i >= 0; i--) {
+        answer.push_back(str[i] - '0');
+    }
+    return answer;
+}
